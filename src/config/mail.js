955c0600bd93783d1d5397a83a7659cba9926f53
CodeWithHimz <codewithhimz@gmail.com>
2026-07-22 23:45:49 +0530

    OTP Update

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,14 +1,14 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  host: process.env.SMTP_HOST,
-  port: Number(process.env.SMTP_PORT),
-  secure: false,
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
-  requireTLS: true,
 });
 
 transporter.verify((error, success) => {
