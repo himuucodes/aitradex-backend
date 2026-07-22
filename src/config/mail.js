cffc7919a99e766a04f6852af59d1a048aea227a
CodeWithHimz <codewithhimz@gmail.com>
2026-07-23 00:06:31 +0530

    OTP Update 3

--- src/config/mail.js
+++ src/config/mail.js
@@ -4,17 +4,8 @@ const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
     user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASS, // Google App Password
+    pass: process.env.SMTP_PASS,
   },
-});
-
-transporter.verify((err, success) => {
-  if (err) {
-    console.error("SMTP VERIFY ERROR:");
-    console.error(err);
-  } else {
-    console.log("SMTP CONNECTED");
-  }
 });
 
 module.exports = transporter;
