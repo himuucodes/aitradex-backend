9282d2425386756e4323144bfcfe398e05537ffe
CodeWithHimz <codewithhimz@gmail.com>
2026-07-23 17:59:01 +0530

    Captcha added

--- src/services/turnstile.service.js
+++ src/services/turnstile.service.js
@@ -1,23 +1,27 @@
 const axios = require("axios");
 
 exports.verifyTurnstile = async (token) => {
-  try {
-    const response = await axios.post(
-      "https://challenges.cloudflare.com/turnstile/v0/siteverify",
-      new URLSearchParams({
-        secret: process.env.TURNSTILE_SECRET_KEY,
-        response: token,
-      }),
-      {
-        headers: {
-          "Content-Type":
-            "application/x-www-form-urlencoded",
-        },
-      }
-    );
+    try {
+        const response = await axios.post(
+            "https://challenges.cloudflare.com/turnstile/v0/siteverify",
+            new URLSearchParams({
+                secret: process.env.TURNSTILE_SECRET_KEY,
+                response: token,
+            }),
+            {
+                headers: {
+                    "Content-Type":
+                        "application/x-www-form-urlencoded",
+                },
+            }
+        );
 
-    return response.data;
-  } catch (error) {
-    throw error;
-  }
+        return response.data;
+    } catch (error) {
+        console.error(
+            error.response?.data || error.message
+        );
+
+        throw error;
+    }
 };
