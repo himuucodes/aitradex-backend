61b1c01d5702ad1ff647192c56884302417655c0
CodeWithHimz <codewithhimz@gmail.com>
2026-07-23 14:01:32 +0530

    Added sucess

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,16 +1,21 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail", // Gmail SMTP
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS, // Gmail App Password
+    pass: process.env.EMAIL_PASS,
   },
+  connectionTimeout: 30000,
+  greetingTimeout: 30000,
+  socketTimeout: 30000,
 });
 
-transporter.verify((error, success) => {
-  if (error) {
-    console.error("❌ Gmail SMTP Error:", error);
+transporter.verify((err) => {
+  if (err) {
+    console.error("SMTP Error:", err);
   } else {
     console.log("✅ Gmail SMTP Connected");
   }
